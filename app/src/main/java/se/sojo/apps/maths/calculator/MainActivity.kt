e89f00fbaf5da5be07d227f08ae97c3e10fdf2f8
Sören Johansson <soren@sojo.se>
2026-04-21 23:00:07 +0200

    Initial commit with basic layout

---
+++ app/src/main/java/se/sojo/apps/maths/calculator/MainActivity.kt
@@ -0,0 +1,60 @@
+package se.sojo.apps.maths.calculator
+
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import se.sojo.apps.maths.calculator.ui.theme.SOJOCalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //actionBar?.hide()
+
+        //WindowCompat.enableEdgeToEdge(window)
+
+        //requestWindowFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+
+        //hideStatusBar()
+
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        hideSystemUI()
+
+        setContentView(R.layout.activity_main)
+
+    }
+
+    private fun hideStatusBar() {
+        WindowCompat.getInsetsController(window,window.decorView).apply {
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            hide(WindowInsetsCompat.Type.statusBars())
+        }
+    }
+
+    fun hideSystemUI() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, findViewById(android.R.id.content)).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+
+}

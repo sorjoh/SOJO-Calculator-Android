818f7137dabcfde41e077fbc80cef2d11375df04
Sören Johansson <soren@sojo.se>
2026-04-29 20:45:57 +0200

    Removed UI Theme and cleaned up some code

--- app/src/main/java/se/sojo/apps/maths/calculator/MainActivity.kt
+++ app/src/main/java/se/sojo/apps/maths/calculator/MainActivity.kt
@@ -1,24 +1,10 @@
 package se.sojo.apps.maths.calculator
 
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.Window
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
-import se.sojo.apps.maths.calculator.ui.theme.SOJOCalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
